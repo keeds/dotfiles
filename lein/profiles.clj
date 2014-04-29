b3bea7098cde1218fdec956fb27fa6d352cafb44
Andrew Keedle <andrew@numptyheads.com>
2014-04-29 12:25:58 +0100

    profiles.clj (codox, cider/cider-nrepl)

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user
- {:plugins [;;[lein-ancient "0.5.1" :exclusions [rewrite-clj common-codec commons-codec]]
-            [lein-ancient "0.5.1"]
-            [lein-kibit "0.0.8"]]}}
+ {:plugins [[lein-ancient "0.5.5"]
+            [lein-kibit "0.0.8"]
+            [codox "0.6.8"]
+            [cider/cider-nrepl "0.6.0"]]}}
