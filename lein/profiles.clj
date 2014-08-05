eadc34e397904d05cfdc555c8b8164c1f8c4fb56
Andrew Keedle <andrew@numptyheads.com>
2014-08-05 20:52:31 +0100

    cider-nrepl 0.7.0

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[lein-ancient "0.5.5"]
-            [lein-kibit "0.0.8"]
-            [codox "0.6.8"]
-            [cider/cider-nrepl "0.6.0"]
-            [lein-marginalia "0.7.1"]]}}
+            [cider/cider-nrepl "0.7.0"]
+            ;; [lein-kibit "0.0.8"]
+            ;; [codox "0.6.8"]
+            ;; [lein-marginalia "0.7.1"]
+            ]}}
