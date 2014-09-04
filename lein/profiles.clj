380fc56232d6dbf954b7e2e3648b6127ea41ee5e
Andrew Keedle <andrew@numptyheads.com>
2014-09-04 16:46:07 +0100

    rejig profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,12 @@
 {:user
  {:plugins [[lein-ancient "0.5.5"]
             [cider/cider-nrepl "0.7.0"]
-            ;; [lein-kibit "0.0.8"]
-            ;; [codox "0.6.8"]
-            ;; [lein-marginalia "0.7.1"]
-            ]}}
+            [lein-cloverage "1.0.2"]
+            [com.jakemccrary/lein-test-refresh "0.5.1"]
+            [lein-marginalia "0.8.0"]]
+  :dependencies [[radagast "2.0.1"]
+                 [pjstadig/humane-test-output "0.6.0"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
+  }
+ }
